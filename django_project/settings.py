4b1364ad9103e5b8cbb395fdfbdfbb38831cc2f1
Sugam Thapa <tsugam74@gmail.com>
2025-09-27 15:33:37 +0545

    'ch-13'

--- django_project/settings.py
+++ django_project/settings.py
@@ -158,12 +158,15 @@ ACCOUNT_LOGOUT_ON_GET = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR / "static"]
 STATIC_ROOT= BASE_DIR/ "staticfiles"
 STATICFILES_STORAGE="django.contrib.staticfiles.storage.StaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+MEDIA_URL='/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -7,4 +9,6 @@ urlpatterns = [
     #local apps :
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
-]
+]+static(
+    settings.MEDIA_URL,document_root=settings.MEDIA_ROOT
+)
