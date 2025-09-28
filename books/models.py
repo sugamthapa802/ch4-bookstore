056468246ce4eef9c8cd85a0712cb57d528ba340
Sugam Thapa <tsugam74@gmail.com>
2025-09-28 12:54:07 +0545

    'ch-14'

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-User=get_user_model()
+# User=get_user_model()
 class Book(models.Model):
     id=models.UUIDField(primary_key=True,
                         default=uuid.uuid4,
@@ -12,6 +12,11 @@ class Book(models.Model):
     author=models.CharField(max_length=100)
     price=models.DecimalField(max_digits=6,decimal_places=2)
     image=models.ImageField(upload_to='covers/',blank=True)
+
+    class Meta:
+        permissions=[
+            ("special_status","Can read all books"),
+        ]
 
     def __str__(self):
         return self.title
--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import Permission
 from django.urls import reverse
 from .models import Book,Review
 from django.contrib.auth import get_user_model
@@ -12,6 +13,9 @@ class Booktests(TestCase):
                                             
                                             )
         cls.book =Book.objects.create(title='book1',author='harry',price='29.00')
+        cls.special_permission=Permission.objects.get(
+            codename='special_status'
+        )
         cls.reviewbook=Review.objects.create(
             book=cls.book,
             review='Excellent book',
@@ -24,13 +28,17 @@ class Booktests(TestCase):
         self.assertEqual(f'{self.book.author}','harry')
         self.assertEqual(f'{self.book.price}','29.00')
     
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user_with_permission(self):
+        self.client.login(username="reviewuser@gmail.com",password='lofi*321')
+        self.user.user_permissions.add(self.special_permission)
         response=self.client.get(reverse('book_list'))
         self.assertEqual(response.status_code,200)
         self.assertContains(response,'book1')
         self.assertTemplateUsed(response,'books/book_list.html')
     
-    def test_book_detail_view(self):
+    def test_book_detail_view_for_logged_in_user_with_permission(self):
+        self.client.login(username="reviewuser@gmail.com",password='lofi*321')
+        self.user.user_permissions.add(self.special_permission)
         response=self.client.get(self.book.get_absolute_url())
         no_response=self.client.get('book/12345')
         self.assertEqual(response.status_code,200)
