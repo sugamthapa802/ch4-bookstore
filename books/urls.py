cd2a031faddd7300df95b946a3e3270e174f3204
Sugam Thapa <tsugam74@gmail.com>
2025-09-28 14:14:41 +0545

    'ch-15'

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListView,BookDetailView
+from .views import BookListView,BookDetailView,SearchBookView
 urlpatterns = [
     path('booklist',BookListView.as_view(),name='book_list'),
-    path('book/<uuid:pk>/',BookDetailView.as_view(),name='book_detail')
+    path('book/<uuid:pk>/',BookDetailView.as_view(),name='book_detail'),
+    path('search/',SearchBookView.as_view(),name='search_book'),
     
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse
 from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
 from django.views.generic import ListView,DetailView
 from .models import Book
+from django.db.models import Q
 
 class BookListView(LoginRequiredMixin,PermissionRequiredMixin,ListView):
     model=Book
@@ -16,3 +17,15 @@ class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
     template_name='books/book_detail.html'
     login_url='account_login'
     permission_required='books.special_status'
+
+class SearchBookView(ListView):
+    model=Book
+    context_object_name='book_list'
+    template_name='books/search_book.html'
+
+    def  get_queryset(self):
+        query=self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query)| Q(author__icontains=query)
+            )
+    
